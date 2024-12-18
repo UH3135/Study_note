54c35bf23c0ef2fff4ad7d407ac4065143a851cf
UH3135 <rabbitdy0101@kookmin.ac.kr>
2024-12-18 14:43:09 +0900

    신경망

---
+++ computer_vision/perseptron.py
@@ -0,0 +1,7 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.datasets as ds
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.
